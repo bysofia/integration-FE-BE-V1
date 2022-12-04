856ba0491180748be27d4b584109cadff80e7df5
unknown <rizkyabdulah.business@gmail.com>
2022-12-05 01:09:15 +0700

    first push

---
+++ server/dto/auth/auth.go
@@ -0,0 +1,27 @@
+package authdto
+
+type RegisterRequest struct {
+	Name     string `json:"name" gorm:"type: varchar(255) required"`
+	Email    string `json:"email" gorm:"type: varchar(255) required"`
+	Password string `json:"password" gorm:"type: varchar(255) required"`
+	Role     string `json:"role" gorm:"type: varchar(255) required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" gorm:"type: varchar(255) required"`
+	Password string `json:"password" gorm:"type: varchar(255) required"`
+}
+
+type LoginResponse struct {
+	Name  string `json:"name" gorm:"type: varchar(255)"`
+	Email string `json:"email" gorm:"type: varchar(255)"`
+	Token string `json:"token" gorm:"type: varchar(255)"`
+	Role  string `gorm:"type: varchar(50)"  json:"role"`
+}
+
+type CheckAuthResponse struct {
+	Id    int    `gorm:"type: int" json:"id"`
+	Name  string `gorm:"type: varchar(255)" json:"name"`
+	Email string `gorm:"type: varchar(255)" json:"email"`
+	Role  string `gorm:"type: varchar(50)"  json:"role"`
+}
